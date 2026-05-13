7408296830cb2061f7ad650a0a90089ff538b000
Ben Christie <benchristie@Bens-MacBook-Pro.local>
2026-05-13 10:23:45 +1000

    rask 0.1.0: set tarball sha256

--- Formula/rask.rb
+++ Formula/rask.rb
@@ -2,7 +2,7 @@ class Rask < Formula
   desc "Internal CLI for Rask Australia (YouTube chapter generation, etc.)"
   homepage "https://github.com/benchristie02/rask-cli"
   url "https://github.com/benchristie02/rask-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_SHA256_OF_TARBALL"
+  sha256 "4935d83c34377bd022188f42d8ec22ac5f80cade78cc19fdf3d56fbcc2779e71"
   version "0.1.0"
   license "MIT"
 
